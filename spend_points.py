e23837a06cfd64483113f646987fbf1df5e19dd2
Wei-Yu <jcab1688@gmail.com>
2023-02-07 01:57:26 -0600

    Update code

--- spend_points.py
+++ spend_points.py
@@ -4,7 +4,7 @@ import traceback
 from typing import List, Tuple
 from collections import defaultdict
 
-CSV_FILEPATH = "test_csv/demo.csv"
+CSV_FILEPATH = "test_csv/demo2.csv"
 
 
 def read_csv(file_path: str):
@@ -37,24 +37,58 @@ def read_csv(file_path: str):
     return records
 
 
-def val_records(records: List[Tuple]):
+def preprocess_records(records: List[Tuple]):
     """
+    Sort the records based on timestamp
     Examine the balance of each payer at every timestamp. Make sure that their balance would not less than 0.
+    Preprocess the records by spending all the negative points in the records.
 
     Args:
         records: list of record
     Return:
-        bool
+        records after processing
     """
 
-    balance = defaultdict(lambda: 0)
-    for record in records:
+    # Sort the CSV based on timestamp
+    records.sort(key=lambda r: r[2])
+
+    # Preprocess
+    for i in range(len(records)):
+        record = records[i]
         payer = record[0]
         points = record[1]
-        balance[payer] += points
-        if balance[payer] < 0:
-            return False
-    return True
+
+        # check if we can pay 'points' amount of points that comes from the 'payer'
+        if points < 0:
+            points_needed_sent = points * -1
+            for j in range(0, i):
+                prev_record = records[j]
+                prev_payer = prev_record[0]
+                prev_points = prev_record[1]
+
+                # find points that comes from this 'payer'
+                if prev_payer == payer:
+                    if prev_points >= points:
+                        prev_points = prev_points - points_needed_sent
+                        points_needed_sent = 0
+                    else:
+                        points_needed_sent -= prev_points
+                        prev_points = 0
+
+                    # update prev record
+                    # (prev_payer, prev_points, timestamp)
+                    records[j] = (prev_payer, prev_points, prev_record[2])
+
+                    if points_needed_sent == 0:
+                        break
+            
+            if points_needed_sent == 0:
+                # This negative points can be processed
+                # update the record
+                records[i] = (payer, 0, record[2])
+            else:
+                raise ValueError("This CSV records violate the rule")
+    return records
 
 
 def cal_result(records: List[Tuple], spend: int):
@@ -89,13 +123,8 @@ def main(spend: int):
         # Read CSV
         records = read_csv(CSV_FILEPATH)
 
-        # Sort the CSV based on timestamp
-        records.sort(key=lambda r: r[2])
-
-        # Validate the records
-        correct = val_records(records)
-        if not correct:
-            raise ValueError("This CSV records violate the rule")
+        # Preprocess the records
+        records = preprocess_records(records)
 
         # Calculate the result
         remain, result = cal_result(records, spend)
--- spend_points_test.py
+++ spend_points_test.py
@@ -1,22 +1,46 @@
 import unittest
-from spend_points import read_csv, val_records, cal_result
+from copy import deepcopy
+from spend_points import read_csv, preprocess_records, cal_result
 
 
 TEST_CASE1 = {
-    "true_records": [
+    "records": [
         ("DANNON", 1000, "2020-11-02T14:00:00Z"),
         ("UNILEVER", 200, "2020-10-31T11:00:00Z"),
         ("DANNON", -200, "2020-10-31T15:00:00Z"),
         ("MILLER COORS", 10000, "2020-11-01T14:00:00Z"),
-        ("DANNON", 300, "2020-10-31T10:00:00Z"),
+        ("DANNON", 300, "2020-10-31T10:00:00Z")
     ],
-    "true_sorted_records": [
-        ("DANNON", 300, "2020-10-31T10:00:00Z"),
+    "p_records": [
+        ("DANNON", 100, "2020-10-31T10:00:00Z"),
         ("UNILEVER", 200, "2020-10-31T11:00:00Z"),
-        ("DANNON", -200, "2020-10-31T15:00:00Z"),
+        ("DANNON", 0, "2020-10-31T15:00:00Z"),
         ("MILLER COORS", 10000, "2020-11-01T14:00:00Z"),
         ("DANNON", 1000, "2020-11-02T14:00:00Z"),
     ],
+    "spend": 5000,
+    "result": {
+        "DANNON": 1000, "UNILEVER": 0, "MILLER COORS": 5300
+    }
+}
+
+TEST_CASE2 = {
+    "records": [
+        ("A",100,"2020-10-31T10:00:00Z"),
+        ("B",100,"2020-10-31T10:01:00Z"),
+        ("A",-100,"2020-10-31T10:02:00Z"),
+        ("A",100,"2020-10-31T10:03:00Z")
+    ],
+    "p_records": [
+        ("A",0,"2020-10-31T10:00:00Z"),
+        ("B",100,"2020-10-31T10:01:00Z"),
+        ("A",0,"2020-10-31T10:02:00Z"),
+        ("A",100,"2020-10-31T10:03:00Z")
+    ],
+    "spend": 100, 
+    "result": {
+        "A": 100, "B": 0
+    }
 }
 
 
@@ -35,28 +59,46 @@ class TestReadCSV(unittest.TestCase):
 
     def test_read_data(self):
         records = read_csv("test_csv/demo.csv")
-        self.assertEqual(records, TEST_CASE1["true_records"])
+        self.assertEqual(records, deepcopy(TEST_CASE1["records"]))
 
 
-class TestValBalance(unittest.TestCase):
+class TestPreprocessRecords(unittest.TestCase):
     def test_violate_rule(self):
-        correct = val_records([("DANNON", -200, "2020-10-31T15:00:00Z")])
-        self.assertEqual(correct, False)
-        correct = val_records(TEST_CASE1["true_sorted_records"])
-        self.assertEqual(correct, True)
+        with self.assertRaises(ValueError):
+            preprocess_records([("DANNON", -200, "2020-10-31T15:00:00Z")])
+        
+    def test_preprocess(self):
+        # TESTCASE 1
+        records = preprocess_records(deepcopy(TEST_CASE1["records"]))
+        self.assertEqual(records, TEST_CASE1["p_records"])
+
+        # TESTCASE 2
+        records = preprocess_records(deepcopy(TEST_CASE2["records"]))
+        self.assertEqual(records, TEST_CASE2["p_records"])
 
 
 class TestCalResult(unittest.TestCase):
     def test_unenough_point(self):
-        remain, _ = cal_result(TEST_CASE1["true_sorted_records"], spend=11300)
+        # TESTCASE 1
+        remain, _ = cal_result(deepcopy(TEST_CASE1["p_records"]), spend=11300)
         self.assertLessEqual(0, remain)
-        remain, _ = cal_result(TEST_CASE1["true_sorted_records"], spend=15000)
+        remain, _ = cal_result(deepcopy(TEST_CASE1["p_records"]), spend=15000)
+        self.assertLessEqual(0, remain)
+
+        # TESTCASE 2
+        remain, _ = cal_result(deepcopy(TEST_CASE2["p_records"]), spend=200)
         self.assertLessEqual(0, remain)
 
     def test_enough_point(self):
-        remain, result = cal_result(TEST_CASE1["true_sorted_records"], spend=5000)
+        # TESTCASE 1
+        remain, result = cal_result(deepcopy(TEST_CASE1["p_records"]), spend=TEST_CASE1["spend"])
         self.assertEqual(remain, 0)
-        self.assertEqual(result, {"DANNON": 1000, "UNILEVER": 0, "MILLER COORS": 5300})
+        self.assertEqual(result, TEST_CASE1["result"])
+
+        # TESTCASE 2
+        remain, result = cal_result(deepcopy(TEST_CASE2["p_records"]), spend=TEST_CASE2["spend"])
+        self.assertEqual(remain, 0)
+        self.assertEqual(result, TEST_CASE2["result"])
 
 
 if __name__ == "__main__":
